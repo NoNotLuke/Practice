3a7d6887d92d27b9d3c45561f17d8bb00d1830aa
NoNotLuke <lmlafave5@gmail.com>
2018-03-02 17:59:35 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var a = 0;
+var elements = document.getElementsByClassName(a);
+function showNext() {
+  for(var i = 0; i <= elements.length; i ++) {
+    elements[i].style.display = 'inline-block';
+  }
+  a = a + 1;
+  elements = document.getElementsByClassName(a);
+}

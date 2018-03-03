7c28da22756392a90fdc0f2dd5e44187faba7a59
NoNotLuke <lmlafave5@gmail.com>
2018-03-02 21:50:26 -0500

    Further improvements.
    A few more decorations, and changed display style to 'block'.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var a = 0;
 var elements = document.getElementsByClassName(a);
 function showNext() {
   for(var i = 0; i < elements.length; i ++) {
-    elements[i].style.display = 'inline-block';
+    elements[i].style.display = 'block';
   }
   a = a + 1;
   elements = document.getElementsByClassName(a);
